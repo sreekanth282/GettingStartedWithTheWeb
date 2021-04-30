59f5d1bd15f126e81d3c3071549d8bd15e870a32
sreekanth <sreekanth.reddy.kota.282@gmail.com>
2021-04-30 19:29:30 +0200

    Getting Started with Web

---
+++ scripts/main.js
@@ -0,0 +1,27 @@
+let myImage = document.querySelector('img');
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute("src");
+    if(mySrc === 'images/firefox-logo.png'){
+        myImage.setAttribute('src','images/chrome-logo.jpg');
+    }
+    else{
+        myImage.setAttribute('src','images/firefox-logo.png');
+    }
+}
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Mozilla is cool, '+ myName;
+}
+if(!localStorage.getItem('name')) {
+    setUserName();
+}
+else{
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+myButton.onclick = function() {
+    setUserName();
+}
